d84bad9a7899fbaf05209d51194cc342481a8d79
MoloZzz <mega_oleksii@ukr.net>
2025-08-31 17:20:07 +0300

    chore: init modules

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,15 @@
+import * as dotenv from 'dotenv';
+dotenv.config();
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { OpenAiModule } from './integrations/open-ai/open-ai.module';
+import { FeedbackModule } from './feedback/feedback.module';
+import { AnalyseModule } from './analyse/analyse.module';
+import { GoogleSheetsModule } from './integrations/google-sheets/google-sheets.module';
 
 @Module({
-  imports: [],
+  imports: [OpenAiModule, FeedbackModule, AnalyseModule, GoogleSheetsModule],
   controllers: [AppController],
   providers: [AppService],
 })
