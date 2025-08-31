de9acc7f3d1225dac0b4034d04ec6a50f51e461e
MoloZzz <mega_oleksii@ukr.net>
2025-08-31 17:26:02 +0300

    chore: init telegram bot integration module

--- src/app.module.ts
+++ src/app.module.ts
@@ -7,9 +7,10 @@ import { OpenAiModule } from './integrations/open-ai/open-ai.module';
 import { FeedbackModule } from './feedback/feedback.module';
 import { AnalyseModule } from './analyse/analyse.module';
 import { GoogleSheetsModule } from './integrations/google-sheets/google-sheets.module';
+import { TelegramBotModule } from './integrations/telegram-bot/telegram-bot.module';
 
 @Module({
-  imports: [OpenAiModule, FeedbackModule, AnalyseModule, GoogleSheetsModule],
+  imports: [OpenAiModule, FeedbackModule, AnalyseModule, GoogleSheetsModule, TelegramBotModule],
   controllers: [AppController],
   providers: [AppService],
 })
